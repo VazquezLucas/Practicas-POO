ce485777b6ace73f6c282e11a96151f8c911ff63
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-03-02 15:36:56 -0300

    practicas poo
    02-03-2023

--- Program.cs
+++ Program.cs
@@ -6,17 +6,25 @@ namespace PracticasPOO
     {
         public static void Main(string[] args)
         {
-
+            
             Circulo miCirculo; //creacion de objeto de tipo circuo. Variable de tipo círculo.
 
             miCirculo = new Circulo(); //Iniciacion de variable/objeto de tipo circulo. Instaciar una clase.
             //Instanciación. Ejemplarizacion. Creación de ejemplar de clase.
 
-            Console.WriteLine(miCirculo.calculoArea(5));
+            Console.WriteLine(miCirculo.CalculoArea(5));
 
             Circulo miCirculo2 = new Circulo();
 
-            Console.WriteLine(miCirculo2.calculoArea(9));
+            Console.WriteLine(miCirculo2.CalculoArea(9));
+
+            Console.WriteLine(miCirculo.CalculoArea(10));
+            
+            ConversorEuroDolar obj = new ConversorEuroDolar();
+
+            obj.cambiarValorEuro(1.45);
+
+            Console.WriteLine(obj.convierte(50));
 
         }
     }
@@ -24,9 +32,9 @@ namespace PracticasPOO
     class Circulo
     {
 
-        const double pi = 3.1416;     //propiedad de la clase Circulo. campo de clase.
+        private const double pi = 3.1416;     //propiedad de la clase Circulo. campo de clase.
 
-        public double calculoArea(int radio)   //método de clase. ¿que pueden hacer los objetos de tipo círculo?.
+        public double CalculoArea(int radio)   //método de clase. ¿que pueden hacer los objetos de tipo círculo?.
         {
 
             return pi * radio * radio;  
@@ -34,4 +42,26 @@ namespace PracticasPOO
         }
 
     }
+
+    class ConversorEuroDolar
+    {
+
+        private double euro = 1.253;
+
+        public double convierte(double cantidad)
+        {
+
+            return cantidad * euro;
+
+        }
+         public void cambiarValorEuro(double nuevoValor)
+        {
+            if (nuevoValor < 0) euro = 1.253;
+
+            else
+                euro = nuevoValor;
+
+        }
+    }
+
 }
